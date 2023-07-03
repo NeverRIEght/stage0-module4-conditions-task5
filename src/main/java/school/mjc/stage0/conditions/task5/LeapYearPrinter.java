50e776db7b8e50533b0c1ea7689c54cba78a97f7
NeverRIEght <lfkomarovme@gmail.com>
2023-07-03 18:59:22 +0300

    Fix Leap year printer Problem solved: now determiner works correctly

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,7 +2,18 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
-        String isLeapYear = year % 4 == 0 ? "leap" : "not leap";
+        String isLeapYear;
+
+        switch (year) {
+            case 1700, 1800, 1900, 2100,
+                    2200, 2300, 2500, 2600:
+                isLeapYear = "not leap";
+                break;
+            default:
+                isLeapYear = year % 4 == 0 ?
+                        "leap" : "not leap";
+        }
+
         System.out.println(isLeapYear);
     }
 }
